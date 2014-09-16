475d6c876a195201fa104ab67e2c1b4267aa121c
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-09-16 11:53:53 +0300

    Fix file order

--- static/assets/js/codebrowser.js
+++ static/assets/js/codebrowser.js
@@ -2119,6 +2119,54 @@ codebrowser.collection.TagCollection = Backbone.Collection.extend({
 });
 ;
 
+codebrowser.view.ListBaseView = Backbone.View.extend({
+
+    events: {
+
+        'click [data-action="search"]': 'filterListByName',
+        'keyup [data-id="query-string"]': 'filterListByName'
+
+    },
+
+    /* Render */
+
+    render: function () {
+
+        // Template
+        var output = this.renderTemplate();
+
+        this.$el.html(output);
+
+        // Bind events also on re-render
+        this.delegateEvents();
+    },
+
+    update: function () {
+
+        var output = this.renderTemplate(),
+            filteredList = $(output).find('table');
+
+        this.$el.find('table').replaceWith(filteredList);
+    },
+
+    /* Filter */
+
+    filterListByName: function () {
+
+        if (!this.filterHelper) {
+            this.filterHelper = new codebrowser.helper.ListViewFilter({ 'containerSelector': '#' + this.id }, this.collection);
+        }
+
+        var result = this.filterHelper.filter();
+
+        this.collection = result.filteredCollection;
+        this.query = result.query;
+
+        this.update();
+    }
+});
+;
+
 codebrowser.view.CoursesView = codebrowser.view.ListBaseView.extend({
 
     id: 'courses-container',
@@ -2803,54 +2851,6 @@ codebrowser.view.InstanceView = codebrowser.view.ListBaseView.extend({
     renderTemplate: function () {
 
         return this.template({ instance: this.instance.toJSON() });
-    }
-});
-;
-
-codebrowser.view.ListBaseView = Backbone.View.extend({
-
-    events: {
-
-        'click [data-action="search"]': 'filterListByName',
-        'keyup [data-id="query-string"]': 'filterListByName'
-
-    },
-
-    /* Render */
-
-    render: function () {
-
-        // Template
-        var output = this.renderTemplate();
-
-        this.$el.html(output);
-
-        // Bind events also on re-render
-        this.delegateEvents();
-    },
-
-    update: function () {
-
-        var output = this.renderTemplate(),
-            filteredList = $(output).find('table');
-
-        this.$el.find('table').replaceWith(filteredList);
-    },
-
-    /* Filter */
-
-    filterListByName: function () {
-
-        if (!this.filterHelper) {
-            this.filterHelper = new codebrowser.helper.ListViewFilter({ 'containerSelector': '#' + this.id }, this.collection);
-        }
-
-        var result = this.filterHelper.filter();
-
-        this.collection = result.filteredCollection;
-        this.query = result.query;
-
-        this.update();
     }
 });
 ;
