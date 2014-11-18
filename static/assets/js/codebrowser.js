df57e1a46a268282f9905f759351d2a034317a12
Kenny Heinonen <kenny_heinonen@hotmail.com>
2014-11-18 11:42:57 +0200

    Fix condition

--- static/assets/js/codebrowser.js
+++ static/assets/js/codebrowser.js
@@ -4234,7 +4234,7 @@ codebrowser.view.SnapshotsTimelineView = Backbone.View.extend({
         var originX = Math.min(first.attrs.cx, this.paper._viewBox[0]);
 
         // Offset is the same as x-coordinate of first snapshot
-        if (leftOffset === first.attrs.cx) {
+        if (originX === first.attrs.cx && leftOffset === first.attrs.cx) {
             leftOffset = 0;
         }
 
